579b5e60a97167262025a07d0b72d90a96a679a5
AnsMurtaza <108094647+Anschoudary@users.noreply.github.com>
2024-09-10 04:52:29 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='datai',
-    version='1.0',
+    version='1.0.1',
     description='A library for data visualization and cleaning',
     author='M Ans',
     author_email='m.ans.cs@outlook.com',
     packages=find_packages(),
     install_requires=[
-        'pandas==2.0.3',
-        'numpy==1.25.1',
-        'matplotlib==3.8.0',
-        'seaborn==0.12.2',
-        'scikit-learn==1.3.0',
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'setuptools',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
