99975e631d85c236b43e8028bfea57cf3f3730b7
AnsMurtaza <108094647+Anschoudary@users.noreply.github.com>
2024-09-11 01:09:57 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datai',
-    version='1.0.1',
+    version='1.5',
     description='A library for data visualization and cleaning',
     author='M Ans',
     author_email='m.ans.cs@outlook.com',
